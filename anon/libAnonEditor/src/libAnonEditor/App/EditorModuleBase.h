1223f26fa0ea7e87682188c12805eb1879cad997
Jason <jasonchan35@gmail.com>
2025-08-20 03:45:07 +0900

    minor bug fix

--- anon/libAnonEditor/src/libAnonEditor/App/EditorModuleBase.h
+++ anon/libAnonEditor/src/libAnonEditor/App/EditorModuleBase.h
@@ -5,7 +5,7 @@ namespace Anon { namespace Editor {
 class EditorModuleBase : public axNonCopyable {
 public:
 	void create() { onCreate(); }
-	void destory() { onDestroy(); }
+	void destroy() { onDestroy(); }
 
 	virtual void onCreate()  {}
 	virtual void onDestroy() {}
